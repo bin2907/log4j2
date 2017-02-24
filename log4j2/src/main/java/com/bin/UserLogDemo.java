19cfefd5c59cc48fd2cd37e377c73a0fe5160efd
bin2907 <bin2907@gmail.com>
2017-02-24 12:56:17 +0700

    Commit

--- log4j2/src/main/java/com/bin/UserLogDemo.java
+++ log4j2/src/main/java/com/bin/UserLogDemo.java
@@ -1,16 +1,3 @@
-/*
- * Copyright 2011-2016 Nakivo Inc.
- * ALL RIGHTS RESERVED.
- *
- * PROPRIETARY/CONFIDENTIAL.
- *
- * This software is the confidential and proprietary information
- * of Company Inc.
- *
- * You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement
- * you entered into with Company Inc.
- */
 package com.bin;
 
 import com.bin.logging.UserLogService;
